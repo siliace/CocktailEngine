670587023a199523f13733df4f4ec8ccdf8b8578
siliace <benjaminbernard1995@gmail.com>
2026-06-01 15:01:15 +0200

    Improve UnitTest main function

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -9,11 +9,18 @@ using namespace Ck;
 using namespace Ck::Main;
 
 COCKTAIL_DECLARE_LOG_CATEGORY(Catch2, LogLevel::Info);
+
 COCKTAIL_DEFINE_LOG_CATEGORY(Catch2);
 
 ExitCode ApplicationMain(Application* application)
 {
     Catch::Session session;
+#ifndef NDEBUG
+    session.configData().verbosity = Catch::Verbosity::High;
+#endif
+
+    CK_LOG(Catch2, LogLevel::Info, CK_TEXT("Starting Catch2 test session"));
+
 #ifdef COCKTAIL_OS_WINDOWS
     Array<const TextChar*> arguments = application->GetArgv().Transform([](const String& argument) {
         return argument.GetData();
@@ -24,16 +31,27 @@ ExitCode ApplicationMain(Application* application)
     });
 #endif
 
-    int result = session.applyCommandLine(static_cast<int>(arguments.GetSize()), arguments.GetData());
-    if (result != 0)
+    int parseResult = session.applyCommandLine(static_cast<int>(arguments.GetSize()), arguments.GetData());
+    if (parseResult != 0)
     {
         CK_LOG(Catch2, LogLevel::Error, CK_TEXT("Failed to parse application arguments"));
         return ExitCode::GeneralError;
     }
 
-    int failCount = session.run();
-    if (failCount)
-        CK_LOG(Catch2, LogLevel::Warning, CK_TEXT("Tests concluded with %d failed tests"), failCount);
+    int result = session.run();
+    if (result == 0)
+    {
+        CK_LOG(Catch2, LogLevel::Info, CK_TEXT("All tests passed"));
+    }
+    else if (result > 0)
+    {
+        CK_LOG(Catch2, LogLevel::Warning, CK_TEXT("Tests finished with %d failed test(s)"), result);
+    }
+    else
+    {
+        CK_LOG(Catch2, LogLevel::Error, CK_TEXT("Catch2 aborted due to internal error"));
+        return ExitCode::GeneralError;
+    }
 
     return ExitCode::Success;
 }
